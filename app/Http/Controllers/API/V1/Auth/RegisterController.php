99315386fc63be1abde5af8185cf12b3ff0a9679
Mahdi Amereh <mahdieightt@gmail.com>
2023-11-07 10:51:53 +0330

    feat: update RegisterController

---
+++ app/Http/Controllers/API/V1/Auth/RegisterController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\API\V1\Auth;
+
+use App\Contracts\Controller\Api\V1\Auth\RegisterControllerInterface;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller implements RegisterControllerInterface
+{
+}
