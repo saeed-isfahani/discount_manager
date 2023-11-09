249d95a801984ef450af40bfbd636f112c93b3ee
Akrez <akrez.like@gmail.com>
2023-11-10 01:26:42 +0330

    some changes

--- app/Http/Requests/Auth/LoginSendVerifyRequest.php
+++ app/Http/Requests/Auth/LoginSendVerifyRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Auth;
 
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -11,7 +11,7 @@ class LoginSendVerifyRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,7 @@ class LoginSendVerifyRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'mobile' => 'required|number|regex:/[0]{1}[0-9]{10}/',
+            'mobile' => 'required|regex:/[0]{1}[0-9]{10}/',
         ];
     }
 }
