7b2cdb1eebedc3f7a5e5538aeb4cc9bd5259f880
saeed <saeed.com2000@gmail.com>
2023-11-11 18:53:16 +0330

    feat: add checkVerify to registerInterface

--- app/Contracts/Controller/Api/V1/Auth/RegisterControllerInterface.php
+++ app/Contracts/Controller/Api/V1/Auth/RegisterControllerInterface.php
@@ -3,9 +3,11 @@
 namespace App\Contracts\Controller\Api\V1\Auth;
 
 use App\Facades\Response;
+use App\Http\Requests\Auth\CheckVerifyRequest;
 use App\Http\Requests\Auth\SendVerifyRequest;
 
 interface RegisterControllerInterface
 {
     public function sendVerify(SendVerifyRequest $request);
+    public function checkVerify(CheckVerifyRequest $request);
 }
