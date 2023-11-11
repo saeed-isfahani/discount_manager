8b8dd46e7911ec852c1a1f5e2d495086faf3974b
saeed <saeed.com2000@gmail.com>
2023-11-11 17:53:19 +0330

    feat: add check-verify end point

--- app/Http/Controllers/Api/V1/Auth/RegisterController.php
+++ app/Http/Controllers/Api/V1/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\Contracts\Controller\Api\V1\Auth\RegisterControllerInterface;
 use App\Enums\VerificationRequest\VerificationRequestProviderEnum;
 use App\Facades\Response;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\CheckVerifyRequest;
 use App\Http\Requests\Auth\SendVerifyRequest;
 use App\Models\User;
 use App\Models\VerificationRequest;
@@ -25,7 +26,7 @@ class RegisterController extends Controller implements RegisterControllerInterfa
             throw new BadRequestException(__('auth.errors.user_exists'));
         }
 
-        $verificationCode = '12345';
+        $verificationCode = '123456';
         if (app()->environment(['production'])) {
             $verificationCode = rand(10000, 99999);
             try {
@@ -57,5 +58,22 @@ class RegisterController extends Controller implements RegisterControllerInterfa
             ]);
         }
         return Response::status(200)->message(__('auth.messages.code_was_sent'))->send();
+    }
+
+    public function checkVerify(CheckVerifyRequest $request)
+    {
+        $verificationCodeIsValid = VerificationRequest::where('receiver', $request->mobile)
+            ->where('expire_at', '>', Carbon::now())
+            ->where('code', $request->code)
+            ->where('veriffication_at', null)
+            ->first();
+
+        if ($verificationCodeIsValid) {
+            $verificationCodeIsValid->veriffication_at = Carbon::now();
+            $verificationCodeIsValid->save();
+            return Response::status(200)->message(__('auth.messages.mobile_is_just_verified'))->send();
+        }
+
+        throw new BadRequestException(__('auth.errors.mobile_or_code_wrong_or_code_expired'));
     }
 }
---
+++ app/Http/Requests/Auth/CheckVerifyRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CheckVerifyRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "mobile" => ['required', 'numeric', 'regex:/[0]{1}[0-9]{10}/'],
+            "code" => ['required', 'numeric', 'digits:6'],
+        ];
+    }
+}
