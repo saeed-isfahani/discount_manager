4aac54eb5adef4ac50ebe603370d91d643b44a4b
akrez <akrez.like@gmail.com>
2023-11-12 18:43:16 +0330

    interface

--- app/Contracts/Controller/Api/V1/Auth/LoginControllerInterface.php
+++ app/Contracts/Controller/Api/V1/Auth/LoginControllerInterface.php
@@ -2,7 +2,12 @@
 
 namespace App\Contracts\Controller\Api\V1\Auth;
 
+use App\Http\Requests\Auth\LoginCheckVerifyRequest;
+use App\Http\Requests\Auth\LoginSendVerifyRequest;
 
 interface LoginControllerInterface
 {
+    public function checkVerify(LoginCheckVerifyRequest $request);
+
+    public function sendVerify(LoginSendVerifyRequest $request);
 }
--- app/Contracts/Controller/Api/V1/Auth/ProfileControllerInterface.php
+++ app/Contracts/Controller/Api/V1/Auth/ProfileControllerInterface.php
@@ -5,4 +5,5 @@ namespace App\Contracts\Controller\Api\V1\Auth;
 
 interface ProfileControllerInterface
 {
+    public function index();
 }
