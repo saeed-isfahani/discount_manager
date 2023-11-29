4be389b44189c6acd039c6a522212777fc4657f8
akrez <akrez.like@gmail.com>
2023-11-29 16:08:19 +0330

    up++

--- app/Http/Requests/UpdateShopRequest.php
+++ app/Http/Requests/UpdateShopRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use App\Rules\ValidateMobileFormat;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateShopRequest extends FormRequest
 {
@@ -22,7 +24,15 @@ class UpdateShopRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => ['required',],
+            'mobile' => ['required', 'numeric', new ValidateMobileFormat],
+            'licence_number' => ['required', 'alpha_num:ascii'],
+            'shop_number' => ['required',],
+            'province_id' => ['required', Rule::exists('province_cities', 'id')->where('type', 'province')],
+            'city_id' => ['required', Rule::exists('province_cities', 'id')->where('type', 'city')],
+            'category_id' => ['required', Rule::exists('categories', 'id')],
+            'location' => ['required', 'regex:/^[-]?(([0-8]?[0-9])\.(\d+))|(90(\.0+)?),[-]?((((1[0-7][0-9])|([0-9]?[0-9]))\.(\d+))|180(\.0+)?)$/'],
+            'address' => ['required',],
         ];
     }
 }
