3e812c65c7bc6e1a44ba1f5870673f9b34892353
Akrez <akrez.like@gmail.com>
2023-12-01 13:22:20 +0330

    PasswordController

--- app/Http/Controllers/Api/V1/Auth/PasswordController.php
+++ app/Http/Controllers/Api/V1/Auth/PasswordController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\V1\Auth;
 
+use App\Facades\Response;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SetPasswordRequest;
 use App\Models\Password;
@@ -14,10 +15,16 @@ class PasswordController extends Controller
         auth()->user()->update([
             'password' => bcrypt($request->password),
         ]);
+
+        return Response::message('auth.messages.you_have_successfully_set_password')->send();
     }
 
     public function remove(Request $request, Password $password)
     {
-        //
+        auth()->user()->update([
+            'password' => null,
+        ]);
+
+        return Response::message('auth.messages.you_have_successfully_remove_password')->send();
     }
 }
