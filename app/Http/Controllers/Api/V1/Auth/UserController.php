9cdcce10fab9d79cb0cdd75d2b9559476cfb2bb2
saeed <saeed.com2000@gmail.com>
2023-12-10 01:11:27 +0330

    feat: add users filter

--- app/Http/Controllers/Api/V1/Auth/UserController.php
+++ app/Http/Controllers/Api/V1/Auth/UserController.php
@@ -6,6 +6,7 @@ use App\Enums\VerificationRequest\UserStatusEnum;
 use App\Facades\Response;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\PaginateRequest;
+use App\Http\Resources\UserCollection;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 
@@ -13,10 +14,21 @@ class UserController extends Controller
 {
     public function index(PaginateRequest $request)
     {
-        $users = User::orderBy('updated_at', 'DESC')->paginate($request->per_page ?? 5);
+        $users = new User();
+        if ($request->q) {
+            $users = $users->where('full_name', 'LIKE', '%' . $request->q . '%');
+        }
+        if ($request->status) {
+            $users = $users->where('status', $request->status);
+        }
+        if ($request->date) {
+            $users = $users->whereDate('created_at', $request->date);
+        }
+
+        $users = $users->orderBy('updated_at', 'DESC')->paginate($request->per_page ?? 5);
 
         return Response::message('shop.messages.shop_list_found_successfully')
-            ->data(new UserResource($users))
+            ->data(new UserCollection($users))
             ->send();
     }
 
